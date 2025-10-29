e6fe090647eed3bdc6c1800d5a2dd1c7fb0cd12e
Lucas T. Carvalho <lucasteixeira.29@gmail.com>
2025-10-29 09:55:04 -0300

    Primeira parte slide 07

---
+++ FP 07/FP07_01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FP07_01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Insira uma string: ");
+        string stringInserida = Console.ReadLine();
+        int contador = 0;
+        char procurado = ' ';
+        for (int i = 0; i < stringInserida.Length; i++)
+        {
+            if (stringInserida[i] == procurado)
+                contador++;
+        }
+        Console.WriteLine("O item procurado aparece {0} vezes.", contador);
+    }
+}
---
+++ FP 07/FP07_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FP07_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira uma string: ");
+        string stringInserida = Console.ReadLine();
+        int contador = ContaProcurado(stringInserida);
+        Console.WriteLine("O item procurado aparece {0} vezes.", contador);
+    }
+
+    static int ContaProcurado(string inserida)
+    {
+        int contador = 0;
+        
+        for (int i = 0; i < inserida.Length; i++)
+        {
+            if (inserida[i] == 'a' || inserida[i] == 'e' || inserida[i] == 'i' || inserida[i] == 'o' || inserida[i] == 'u' || inserida[i] == 'A' || inserida[i] == 'E' || inserida[i] == 'I' || inserida[i] == 'O' || inserida[i] == 'U')
+                contador++;
+        }
+        return contador;
+    }
+}
