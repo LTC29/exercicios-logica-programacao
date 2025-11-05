2aff528c38b4f2f5f449190dfe2c82c20d3ddd3c
Lucas T. Carvalho <lucasteixeira.29@gmail.com>
2025-11-05 10:32:01 -0300

    Continuando estudos

---
+++ FP 08/FP08_04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FP08_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Tamanho das linhas/colunas da matriz quadrada: ");
+        int tam = Convert.ToInt32(Console.ReadLine());
+        double[,] matriz = new double[tam, tam];
+        for (int i = 0; i < tam; i++)
+        {
+            for (int j = 0; j < tam; j++)
+            {
+                matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        double soma = SomaElementosDaMatriz(matriz, tam);
+        Console.WriteLine("Soma dos elementos da matriz é {0}", soma);
+    }
+
+    static double SomaElementosDaMatriz(double[,] matriz, int tam)
+    {
+        double soma = 0;
+        for (int i = 0; i < tam; i++)
+        {
+            for (int j = 0; j < tam; j++)
+            {
+                soma += matriz[i, j];
+            }
+        }
+        return soma;
+    }
+}
